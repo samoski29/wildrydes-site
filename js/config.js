ca336627d7cf4b46ecf117bcb4ae53ce7dc3eb9f
samoski29 <samoskiclement@gmail.com>
2025-06-05 17:20:17 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_jPi2mWPqK', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3toc2j1smko3arrsrfhk5f144a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_4dMjO9kMx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '9ihe0b23j0991joshh3mgpkpf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://tb3mgmzdq6.execute-api.us-west-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
